50dbc018d8985e34653dcd1cb786e6487e51ba8b
RobinJessicaLittle <robin.jessica.little@outlook.com>
2022-05-16 15:52:01 +0100

    code along

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {useEffect, useState} from "react";
+import {Header} from "./components/headers";
+import {fetchIMages} from "./utils"
+import "./App.css"
+
+
+
+const App = () => {
+    const [userInput, setUserInput] = useState();
+    const [title, setTitle] = useState();
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+      fetchIMages(setImages);
+    }, [])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setTitle(userInput);
+    };
+
+    return(
+        <div className="App">
+            <Header title={title}/>
+            <form onSubmit ={submitHandler}>
+            <input onChange ={(e) => setTitle(e.target.value)} /> 
+            {/* above changes title upon input */}
+            </form>
+            {title ? <h2>You wrote a title</h2> : <h2>Hurry up and Write a title </h2>}
+            {title && <h2>Hooray</h2>}
+            {images.map((image, i) =>{
+              return(
+                <img src = {image.download_url} alt = {`random file from unsplash number ${i}`}/>
+              )
+            })}
+        </div>
+    )
+};
 
 export default App;
